73f4dd8d08be25807dec9e0bebc9cf29363c2912
Alexander Kleymenov <unit@hasmind.net>
2013-05-30 18:16:05 +0400

    toProd

--- lib/active_admin/views/footer.rb
+++ lib/active_admin/views/footer.rb
@@ -11,7 +11,7 @@ module ActiveAdmin
 
       def powered_by_message
         para I18n.t('active_admin.powered_by',
-          :active_admin => link_to("Active Admin", "http://www.activeadmin.info"),
+          :active_admin => link_to("DHARMA Dev.", "http://www.hasmind.net"),
           :version => ActiveAdmin::VERSION).html_safe
       end
 
